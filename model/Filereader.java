c1f08273a7ee8e66d9ee7e0ba6e6c8d3d1a1bc1b
Miranda Holmlund <mirreanda@hotmail.com>
2022-11-17 13:54:27 +0100

    Started with filereader

--- model/Filereader.java
+++ model/Filereader.java
@@ -1,13 +1,28 @@
 package model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Filereader {
+  public String textData;
   
 
-  public void readFile() {
-
+  public void readFile(String fileName) {
+    try {
+      File myObj = new File("../textfile/" + fileName);
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        textData = textData + myReader.nextLine();
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found.");
+      e.printStackTrace();
+    }
   }
 
-  public void getFile() {
-    
+  public String getFileTextAsString() {
+    return textData;
   }
 }
